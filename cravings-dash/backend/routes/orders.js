35e5b422aa454a1fd9d9564571d9ec0b17708591
NRenella <ntrenella@cpp.edu>
2020-02-23 11:39:57 -0800

    temp backend complete

--- cravings-dash/backend/routes/orders.js
+++ cravings-dash/backend/routes/orders.js
@@ -28,4 +28,32 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: '+ err));
 });
 
+router.route('/:id').get((req, res) =>{
+    Order.findById(req.params.id)
+        .then(order => res.json(order))
+        .catch(err => res.status(400).json('Error: '+ err));
+});
+
+router.route('/:id').delete((req, res) =>{
+    Order.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Order Deleted.'))
+        .catch(err => res.status(400).json('Error: '+ err));
+});
+
+router.route('/update/:id').post((req, res) =>{
+    Order.findById(req.params.id)
+        .then(order => {
+            order.username = req.body.username;
+            order.items = req.body.items;
+            order.shipped = Boolean(req.body.shipped);
+            order.totalPrice = Number(req.body.totalPrice);
+            order.date = Date.parse(req.body.date);
+
+            order.save()
+                .then(() => res.json('Order Updated.'))
+                .catch(err => res.status(400).json('Error: '+ err));
+        })
+        .catch(err => res.status(400).json('Error: '+ err));
+});
+
 module.exports = router;
